05e2af89bb3fa9c7971ac27fcc17483246f32ff3
Chun-Wah Chung <chunwahchung@ibm.com>
2021-08-29 16:30:50 -0400

    rlcone copy

--- ibmcloud_cos_backup.sh
+++ ibmcloud_cos_backup.sh
@@ -26,3 +26,12 @@ perform_dry_run() {
     local __dst_cos_bucket=$4
     rclone --dry-run copy $__src_cos_service_instance:$__src_cos_bucket $__dst_cos_service_instance:$__dst_cos_bucket
 }
+
+perform_copy() {
+   
+    local __src_cos_service_instance=$1
+    local __src_cos_bucket=$2
+    local __dst_cos_service_instance=$3
+    local __dst_cos_bucket=$4
+    rclone -v -P copy --checksum $__src_cos_service_instance:$__src_cos_bucket $__dst_cos_service_instance:$__dst_cos_bucket
+}
