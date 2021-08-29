c5b839892af6a2ceabf9db3bf880af1e463b7727
Chun-Wah Chung <chunwahchung@ibm.com>
2021-08-29 16:28:57 -0400

    configure hmac keys

--- ibmcloud_cos_backup.sh
+++ ibmcloud_cos_backup.sh
@@ -8,3 +8,12 @@ ibmcloud_cli_login() {
 set_cos_auth_to_hmac() {
     echo 2 | ibmcloud cos config auth
 }
+
+config_ibmcloud_cli_cos() {
+
+    local __access_key_id=$1
+    local __secret_access_key=$2
+
+    echo "$__access_key_id
+    $__secret_access_key" | ibmcloud cos config hmac
+}
