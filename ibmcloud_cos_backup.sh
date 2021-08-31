a95ae289f4d5b31e1fd3f3667ac11ad092537980
Chun-Wah Chung <chunwahchung@ibm.com>
2021-08-30 20:59:46 -0400

    configure cos hmac before bucket prep

--- ibmcloud_cos_backup.sh
+++ ibmcloud_cos_backup.sh
@@ -38,14 +38,18 @@ perform_copy() {
 
 prepare_backup_bucket() {
 
-    local __dst_cos_service_instance=$1
-    local __bucket=$2
+    local __dst_cos_service_instance_id=$1
+    local __bucket_name=$2
+    local __backup_account_cos_instance_access_key_id=$3
+    local __backup_account_cos_instance_secret_access_key=$4
 
-    rclone lsd $__dst_cos_service_instance: | grep $__bucket > /dev/null 2>&1
+    config_ibmcloud_cli_cos $__backup_account_cos_instance_access_key_id $__backup_account_cos_instance_secret_access_key > /dev/null 2>&1
+
+    rclone lsd $__dst_cos_service_instance_id: | grep $__bucket > /dev/null 2>&1
     local __backup_bucket_exists=$(echo $?)
     if [[ $__backup_bucket_exists -eq 1 ]]; then 
         echo 'bucket not found. creating bucket now..'
-        ibmcloud cos bucket-create --bucket $__bucket
+        ibmcloud cos bucket-create --bucket $__bucket_name
     else
         echo 'backup bucket exists'
     fi
